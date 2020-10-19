8cefe72015a2ee2cba6e10f1fb4f3dbedc8ced85
00008878 <73093027+00008878@users.noreply.github.com>
2020-10-19 10:33:14 +0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,22 @@
+# Example 1
+def age_function(age):
+    print(age)
+
+
+# age_function("10")
+# parameter is the "age" written in parenthesis, where argument is the actual number while calling the function
+
+# Example 2
+var_age = 10
+age_function(var_age)
+# This is variable
+age_function(5+5)
+# and this one is expression
+
+# Example 3
+def local_variable():
+ someVariable = "Hello world"
+ return
+
+# print(someVariable)
+# It won't work because of the scope!
